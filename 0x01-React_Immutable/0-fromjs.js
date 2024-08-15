ae7d6a604db54171b1af7601ba881aaa37f88b38
hassankyanzi <kyanzi.hassan2@gmail.com>
2024-08-15 17:29:54 +0300

    changes made

--- 0x01-React_Immutable/0-fromjs.js
+++ 0x01-React_Immutable/0-fromjs.js
@@ -1,22 +1,5 @@
-// Use CommonJS syntax
-const { fromJS } = require('immutable');
+import { fromJS } from 'immutable';
 
-// Define the getImmutableObject function
-function getImmutableObject(object) {
-    return fromJS(object);
+export default function getImmutableObject(object) {
+  return fromJS(object);
 }
-
-// Export the function
-module.exports = { getImmutableObject };
-
-// Example usage (optional, for testing purposes)
-const obj = {
-    fear: true,
-    smell: -1033575916.9145899,
-    wall: false,
-    thing: -914767132
-};
-
-// Call the function and log the result to the console
-const immutableObject = getImmutableObject(obj);
-console.log(immutableObject);
